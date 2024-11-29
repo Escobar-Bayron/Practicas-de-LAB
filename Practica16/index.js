39276c8f145b53741f93978611be1ec25eb4ca0e
Escobar-Bayron <escobarbayron19@gmail.com>
2024-11-29 12:56:54 -0600

    practicas

---
+++ Practica16/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
